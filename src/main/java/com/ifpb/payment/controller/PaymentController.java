ef923befe6d4a2f3decc43ec2279dfd927165c52
lucasgomes14 <lucazmatehus14@gmail.com>
2025-08-20 10:49:40 -0300

    chore - adicionado o swagger

--- src/main/java/com/ifpb/payment/controller/PaymentController.java
+++ src/main/java/com/ifpb/payment/controller/PaymentController.java
@@ -1,12 +1,23 @@
 package com.ifpb.payment.controller;
 
 import com.ifpb.payment.dto.request.PaymentRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface PaymentController {
+    @Operation(
+            summary = "Fazer pagamento",
+            description = "Recebe no corpo o valor, o método, nome do cliente e email"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pagamento realizado"),
+            @ApiResponse(responseCode = "500", description = "Erro inesperado")
+    })
     @PostMapping("/pay")
     ResponseEntity<Void> pay(@Valid @RequestBody PaymentRequestDTO dto);
 }
